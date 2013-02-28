d450f575ac75e1bbfa039946b0bcff33655bf414
Furukawa Shunta <shunter1112@Furukawa-Shunta-no-MacBook-Pro.local>
2013-02-28 19:07:25 +0900

    Made test case more actual.

--- test/test_ruby_psd.rb
+++ test/test_ruby_psd.rb
@@ -8,13 +8,48 @@ psd = RubyPSD.new("./sample.psd")
 psd.width = 640
 psd.height = 960
 psd.layers = [
-  "group",
+  "[img] background",
   [
-    "group_inner",
-    "group_inner",
-    ["[bsed] array_incum", "anarcy"],
-    "group_inner",
-    "group_inner"
+    "[img] title",
+    [
+      "[btn] edit_button",
+      [ 
+        "content" 
+      ],
+      "content" 
+    ],
+    "[img] pin_animation_background",
+    [
+      "[img] pin_animation",
+      [
+        "[f] frame1",
+        ["content"],
+        "[f] frame2",
+        ["content"],
+        "[f] frame3",
+        ["content"],
+        "[f] frame4",
+        ["content"]
+      ],
+      "content",    
+    ],
+    "[img] animation_label",
+    ["content"],
+    "[img] list_label",
+    ["content"],
+    "[img] checkbox_label",
+    ["content"],
+    "[img] list_background",
+    ["content"],
+    "[pg] progress",
+    [
+      "[knob] knob",
+      ["content"],
+      "filled",
+      "empty"
+    ],
+    "[img] progress_bar_label",
+    ["content"]
   ]
 ]
 
